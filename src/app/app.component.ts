4d363bd5450c4719db920b8b640fe0d498073640
wackdot <e0167324@u.nus.edu>
2018-01-09 16:17:05 +0800

    9/1/18 4:17 PM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,9 @@ export class AppComponent {
 
   constructor() {
     this.articles = [
-      new Article('Angular 4', 'http://angular.io', 0),
-      new Article('Fullstack', 'http://fullstack.io', 0),
-      new Article('Postgresql', 'https://www.postgresql.org/', 0),
+      new Article('Angular 4', 'http://angular.io', 1),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Postgresql', 'https://postgresql.org/', 3),
     ];
   }
 
@@ -22,8 +22,13 @@ export class AppComponent {
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     this.articles.push(new Article(title.value, link.value, 0));
-    title.value = '';
+    title.value = ''; // Refreshes input value to blanks
     link.value = '';
     return false;
   }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes) // => is the arrow function from ES6 (ECMAScript 6)
+  }
+
 }
